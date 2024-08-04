e2823e132a17f749b555524df086d4c66be7b1fa
Manuel CH <manuel_7795@hotmail.com>
2024-08-03 22:19:16 -0700

    Add leetcode graphql source

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,49 @@
 import type { GatsbyConfig } from "gatsby";
+import { buildSchema } from "graphql";
+import fs from "fs";
+import { createHttpLink, from } from "@apollo/client";
+import { RetryLink } from "@apollo/client/link/retry";
+
+const retryLink = new RetryLink({
+  delay: {
+    initial: 100,
+    max: 2000,
+    jitter: true,
+  },
+  attempts: {
+    max: 5,
+    retryIf: (error, operation) =>
+      Boolean(error) && ![500, 400].includes(error.statusCode),
+  },
+})
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Leetcode Playground`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "Leetcode Playground",
+    siteUrl: "https://www.yourdomain.tld"
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        typeName: "Leetcode",
+        fieldName: "leetcode",
+        url: "https://leetcode.com/graphql",
+        createSchema: async () => {
+          const sdl = fs
+            .readFileSync(`${__dirname}/leetcode.gql`)
+            .toString()
+          return buildSchema(sdl)
+        },
+        createLink: (pluginOptions: { url: string }) =>
+          from([retryLink, createHttpLink({ uri: pluginOptions.url })]),
+        // Additional options to pass to node-fetch
+        // fetchOptions: {},
+      },
+    },
+    "gatsby-plugin-postcss"
+  ],
 };
 
 export default config;
